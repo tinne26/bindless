49ebe738ca238ea410a781c425123d17a74038ae
Sergi Llamas <tinne26k@gmail.com>
2022-08-04 17:03:24 +0200

    Set OpenGL to be the default backend on Windows.

--- src/game/windows_opengl.go
+++ src/game/windows_opengl.go
@@ -1,10 +1,11 @@
+//go:build windows
+
 package game
 
 import "os"
 
-// this fix won't be needed after v2.4, but currently directX performance
-// is quite bad and can cause lag when skipping text screens which load
-// many glyphs at once in a single frame
+// currently directX performance is quite bad and can cause lag when
+// skipping text screens which load many glyphs at once in a single frame
 func init() {
 	// allow directX if passed as program flag
 	for _, arg := range os.Args {
